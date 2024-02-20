aa9406c7e9263e8e4be03aab780e04b2f55fdb8a
kumar shanu <kumar.shanu@stockedge.com>
2024-02-20 15:30:30 +0530

    get movie basic details

--- app/controller/movie.controller/movie.controller.js
+++ app/controller/movie.controller/movie.controller.js
@@ -1,7 +1,24 @@
 const { Movie } = require("../../model/movie.model")
 
-exports.getMovie = (req,res) => {
-    console.log('get movie details')
+exports.getMovie = async (req,res) => {
+    try{
+        const movie = await Movie.find({},{
+            _id:1,
+            movieName:1,
+            movieCategory:1,
+            movieDiscription:1,
+            movieImage:1,
+            movieDuriation:1,
+            movieDirector:1
+        });
+        res.status(201).send(movie)
+
+    }catch(e){
+        res.status(500).send({
+            message:'something went wrong ',
+            serverMessage:e.message
+        })
+    }
 }
 exports.postMovie = async (req,res)=> {
     try{
