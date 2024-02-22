cfb98fed8b214b75ba311a245fd5c396f79490af
kumar shanu <kumar.shanu@stockedge.com>
2024-02-22 12:00:56 +0530

    get seat avaliablity of a movie

--- app/controller/movie.controller/movie.controller.js
+++ app/controller/movie.controller/movie.controller.js
@@ -66,3 +66,30 @@ exports.getParticular = async (req,res)=> {
         })
     }
 }
+
+exports.getAvaliableSeat = async (req,res) => {
+    try{
+        let  Movieid = req.params.id;
+        let  date = req.params.date;
+        let time = req.params.time;
+        let updatedDateFormat = `${date.substring(0, 4)}/${date.substring(4, 6)}/${date.substring(6, 8)}`;
+       let updateTime  = `${time.substring(0, 2)}:${time.substring(2, 4)}`
+        const movie = await Movie.findById(Movieid);
+        let totalTicketForASlot = movie.totalTicketForASlot;
+        //get all user booked for a particular date 
+         let userBookedForADate =movie.ticketBookedByUser.filter(booked => booked.MovieSlotDate === updatedDateFormat && booked.MovieSlotTime === updateTime);
+         let seat = [];
+         seat.length=totalTicketForASlot;
+         seat.fill(1);
+        seat= seat.map((el,i) => i).filter(el => !userBookedForADate.find(booked => +(booked.seatNumber) === +(el)));
+        res.status(200).send({
+            'avaliableSeat':seat,
+        });
+    }catch(e){
+        res.status(500).send({
+            message:'something went wrong ',
+            serverMessage:e.message
+        })
+    }
+
+}
--- app/router/movie.router.js
+++ app/router/movie.router.js
@@ -1,4 +1,4 @@
-const { getMovie,postMovie,getParticular} = require("../controller/movie.controller/movie.controller")
+const { getMovie,postMovie,getParticular,getAvaliableSeat} = require("../controller/movie.controller/movie.controller")
 
 module.exports= (app) =>{
     console.log('routing part :')
@@ -6,7 +6,8 @@ module.exports= (app) =>{
     // console.log('routing part :',app)
     app.get('/moviebooking/api/v1/getMovie',getMovie);
     app.get('/moviebooking/api/v1/getParticular/:id',getParticular);
-    app.post('/moviebooking/api/v1/postMovie',postMovie)
+    app.post('/moviebooking/api/v1/postMovie',postMovie);
+    app.get('/moviebooking/api/v1/getAvaliableSeat/:id/:date/:time',getAvaliableSeat)
     // 65d4696c0327226965ea12f8
 
 }
